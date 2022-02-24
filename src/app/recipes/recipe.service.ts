38d9312250ae10cc5a24b47f585caea3eb849113
Vangelis Giannakosian <vangelis@telesto.gr>
2022-02-24 12:23:14 +0200

    Added ingredients to recipes model - adjusted recipe-detail.component.html

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable, Output} from "@angular/core";
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable()
 export class RecipeService {
@@ -7,10 +8,10 @@ export class RecipeService {
   recipeSelected : EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Name','Desc',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU"),
-    new Recipe('Name2','Desc2',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU"),
-    new Recipe('Name3','Desc3',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU"),
-    new Recipe('Name4','Desc4',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU"),
+    new Recipe('Name','Desc',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU",[new Ingredient('Tomatoes',2)]),
+    new Recipe('Name2','Desc2',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU",[new Ingredient('Potatos',2)]),
+    new Recipe('Name3','Desc3',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU",[new Ingredient('Carrots',2)]),
+    new Recipe('Name4','Desc4',"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpQiTum4APhqpBdXiEe2HsaRnw0OGXcPQjsA&usqp=CAU",[new Ingredient('Tomatoes',2)]),
   ];
 
 
