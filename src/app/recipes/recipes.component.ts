0ec789cdc718091203642899f315f6fae43921bc
Vangelis Giannakosian <vangelis@telesto.gr>
2022-03-10 16:25:43 +0200

    added functionality to unsubscribe from subjects

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from "./recipe.model";
 import {RecipeService} from './recipe.service'
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-recipes',
@@ -8,14 +9,19 @@ import {RecipeService} from './recipe.service'
   styleUrls: ['./recipes.component.scss'],
   providers: [RecipeService]
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent implements OnInit, OnDestroy {
 
   activeRecipe: Recipe;
+  private recipeChangeSub: Subscription;
 
   constructor(private recipeService: RecipeService) {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => this.activeRecipe = recipe)
+    this.recipeChangeSub = this.recipeService.recipeSelected.subscribe((recipe: Recipe) => this.activeRecipe = recipe)
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.recipeChangeSub.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "./shopping-list.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private ingredientsChangeSub: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => this.ingredients = ingredients)
+    this.ingredientsChangeSub = this.shoppingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => this.ingredients = ingredients)
+  }
+
+  ngOnDestroy(): void{
+    this.ingredientsChangeSub.unsubscribe();
   }
 }
